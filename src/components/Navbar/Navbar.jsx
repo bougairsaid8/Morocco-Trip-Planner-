9a84c488bdedb18ba086d43ff63f9f234107e49c
tpGit <janalito99@gmail.com>
2026-02-19 11:01:22 +0000

    fix:fixing navbar links and auth

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,20 @@
-import React from 'react';
-import {Link} from "react-router-dom";
-import "./Navbar.css"
-import { useState } from 'react';
-import { LuMenu, LuX } from "react-icons/lu";
-
+import React, { useState } from 'react';
+import { Link } from "react-router-dom";
+import { useSelector } from 'react-redux'; 
+import "./Navbar.css";
+import { LuMenu, LuX, LuLogOut } from "react-icons/lu";
+import { selectIsAuthenticated } from '../../reduxStructure/selectors';
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+  const isAuthenticated = useSelector(selectIsAuthenticated);
 
-const [isOpen, setIsOpen] = useState(false);
-
-
+  const handleLogout = () => {
+    console.log("User Logged Out");
+  };
 
   return (
     <div className='Header'>
-
       <p className='logo'>Morocco Trip Planner</p>
 
       <div className="menu-icon" onClick={() => setIsOpen(!isOpen)}>
@@ -22,17 +23,40 @@ const [isOpen, setIsOpen] = useState(false);
 
       <div className={`nav-wrapper ${isOpen ? 'open' : ''}`}>
         <nav>
-            <Link to="/">Home</Link>
-            <Link to="/explorer">Explorer</Link>
+          <Link className='a' to="/">Home</Link>
+          <Link className='a' to="/explorer">Explorer</Link>
+          <Link className='a' to="/about">About</Link>
+          
+          {/* 🎯 إيلا كان مـسجل، نـزيدو هاد الـ روابط */}
+          {isAuthenticated && (
+            <>
+              <Link className='a' to="/my-trips">My Trips</Link>
+            </>
+          )}
         </nav>
 
         <div className='Navbuttons'>
-            <Link to="/login"><button className='btn'>Login</button></Link>
-            <Link to="/signup"><button className='btn'>Sign in</button></Link>
+          {isAuthenticated ? (
+            /* 👤 حالة الـ مستخدم الـ مـسجل (User Profile) */
+            <div className="user-profile">
+              <Link  to="/profile" className="user-info">
+                <div alt="Avatar" className="avatar" />
+                <span className="user-name">Alex Morgan</span>
+              </Link>
+              <button className='logout-btn' onClick={handleLogout}>
+                <LuLogOut /> <span>Logout</span>
+              </button>
+            </div>
+          ) : (
+            <>
+              <Link to="/login"><button className='btn'>Login</button></Link>
+              <Link to="/signup"><button className='btn signin'>Sign in</button></Link>
+            </>
+          )}
         </div>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
